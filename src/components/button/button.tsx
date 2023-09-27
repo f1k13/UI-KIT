70f8d907fb7fb097f666e3b2a78ba434e9418cc7
Егор <124521323+f1k13@users.noreply.github.com>
2023-09-27 20:42:34 +0300

    feat: anim button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,6 +1,6 @@
 import React, { ButtonHTMLAttributes, useEffect, useRef } from "react";
 import styles from "./button.module.scss";
-const button = ({
+const Button = ({
   children,
   variant = "text",
   ...rest
@@ -8,16 +8,37 @@ const button = ({
   children?: React.ReactNode;
   variant?: "text" | "contained" | "outlined";
 } & ButtonHTMLAttributes<HTMLButtonElement>) => {
-
   const ref = useRef<HTMLButtonElement | null>(null);
 
-
   useEffect(() => {
-
     const applyContainerProperties = () => {
       ref.current?.classList.add("effect");
     };
+
     applyContainerProperties();
+
+    const applyStyles = (e: Event) => {
+      const { offsetY, offsetX } = e;
+      const { style } = ref.current;
+      const sizeOffset = 50;
+      style.setProperty("--effect-top", `${offsetX - sizeOffset}px`);
+      style.setProperty("--effect-top", `${offsetY - sizeOffset}px`);
+    };
+
+    const onClick = (e) => {
+      ref.current?.classList.remove("active");
+      applyStyles(e);
+
+      ref.current?.classList.add("active");
+    };
+
+    ref.current?.addEventListener("mouseup", onClick);
+
+    const cleanupRef = ref.current;
+
+    return () => {
+      cleanupRef?.removeEventListener("mouseup", onClick);
+    };
   }, []);
 
   return (
@@ -27,4 +48,4 @@ const button = ({
   );
 };
 
-export default button;
+export default Button;
